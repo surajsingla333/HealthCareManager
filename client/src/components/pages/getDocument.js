8eeb4bcf6580e7ee7e8d8829d84d40781785e095
Suraj Singla <surajsingla333@gmail.com>
2020-06-28 22:46:25 +0530

    UI

--- client/src/components/pages/getDocument.js
+++ client/src/components/pages/getDocument.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { findDOMNode } from "react-dom";
 
-import { Form, Button } from 'react-bootstrap';
-
+import { Form, Button, Container, Row, Col, Card, ListGroup, Table } from 'react-bootstrap';
 
 import AES from 'crypto-js/aes';
 import enc from 'crypto-js/enc-latin1';
@@ -10,75 +9,86 @@ import ipfs from '../../ipfs';
 
 import { connect } from 'react-redux';
 
-class Download extends Component{
+class Download extends Component {
 
-    componentDidMount(){
+  componentDidMount() {
 
-        console.log("GET DOCUMENTFROM IPFS", this.props);
+    console.log("GET DOCUMENTFROM IPFS", this.props);
 
-    }
+  }
 
-    getFile(e){
-        e.preventDefault();
-        var file = findDOMNode(this.refs.fileHash).value;
-        var element = findDOMNode(this.refs.downloadLink);
-        console.log(file);
-        console.log(element);
+  getFile(e) {
+    e.preventDefault();
+    var file = findDOMNode(this.refs.fileHash).value;
+    var element = findDOMNode(this.refs.downloadLink);
+    console.log(file);
+    console.log(element);
 
-        ipfs.files.get(file.toString(), function(err, res){
-            if(err){
-                console.log("Error", err);
-            }
-            else{
-                console.log("RESPONSE\n", res);
-                console.log("RESPONSE PATH\n",res[0].path);
-                var cont = res[0].content.toString('utf8');
-                console.log("RESPONSE CONTENT\n", cont);
+    ipfs.files.get(file.toString(), function (err, res) {
+      if (err) {
+        console.log("Error", err);
+      }
+      else {
+        console.log("RESPONSE\n", res);
+        console.log("RESPONSE PATH\n", res[0].path);
+        var cont = res[0].content.toString('utf8');
+        console.log("RESPONSE CONTENT\n", cont);
 
-                // var n = cont.slice(30, cont.length)
+        // var n = cont.slice(30, cont.length)
 
-                // console.log(n);
-                // console.log(cont[20:cont.length()])
-                // data:application/octet-stream,
-                var decrypted = AES.decrypt(cont.toString(), "qwertyuiop").toString(enc);
+        // console.log(n);
+        // console.log(cont[20:cont.length()])
+        // data:application/octet-stream,
+        var decrypted = AES.decrypt(cont.toString(), "qwertyuiop").toString(enc);
 
-                console.log("DECRYPT\n", decrypted);
-                console.log("\nDECRYPT\n", decrypted.toString());
+        console.log("DECRYPT\n", decrypted);
+        console.log("\nDECRYPT\n", decrypted.toString());
 
-                element.setAttribute('href', decrypted);
-				element.setAttribute('download', "FILE_DECRYPTED");
-            }
-        })
+        element.setAttribute('href', decrypted);
+        element.setAttribute('download', "FILE_DECRYPTED");
+      }
+    })
 
-    }
+  }
 
-    render(){
-        return(
-            <div style={{marginLeft:"10px", paddingRight:"50px"}}>
-                <h1>Download Data File</h1>
+  render() {
+    return (
 
-                <Form onSubmit={this.getFile.bind(this)}>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Download Data File</Card.Title>
+                <Card.Text>
+                  <Form onSubmit={this.getFile.bind(this)}>
 
                     <Form.Group controlId="fileHash">
-                        <Form.Label>File Hash</Form.Label>
-                        <Form.Control type="text" placeholder="Enter File Hash" ref='fileHash' />
+                      <Form.Label>File Hash</Form.Label>
+                      <Form.Control type="text" placeholder="Enter File Hash" ref='fileHash' />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">Get File</Button>
 
-                    <Button variant="success" style={{marginLeft:"5px"}}><a ref="downloadLink"> Click here to download</a></Button>
+                    <Button variant="success" style={{ marginLeft: "5px" }}><a ref="downloadLink"> Click here to download</a></Button>
 
-                </Form>
-                <small style={{marginLeft:"5px"}}>If file type is not automatically detected, then save it with '.docx' extention</small>
-            </div>
-        )
-    }
+                  </Form>
+                  <small style={{ marginLeft: "5px" }}>If file type is not automatically detected, then save it with '.docx' extention</small>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        state
-    }
+  return {
+    state
+  }
 }
 
 export default connect(mapStateToProps)(Download);
