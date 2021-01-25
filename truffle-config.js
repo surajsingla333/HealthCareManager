2c22d4f5dbdb17ec49f03b4865947b40c5a6b3b7
Suraj Singla <surajsingla333@gmail.com>
2021-01-25 13:07:30 +0530

    updated functions and installation

--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,7 @@ module.exports = {
     // }
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8456,           // Standard Ethereum port (default: none)
+      port: 9911,           // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
     matic: {
